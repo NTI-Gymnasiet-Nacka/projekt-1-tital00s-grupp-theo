9f5ff8bfb8f35d12cb3c78bb69ca36f3ac68a186
MusseB <maximus.brandt@elev.ga.ntig.se>
2024-01-31 11:16:05 +0100

    stop

--- classes.py
+++ classes.py
@@ -2,7 +2,6 @@ class Workout:
     def __init__ (self, name: str, weight: float, reps: int, sets: int):
         self.name: str = name
         self.weight: float = weight
-<<<<<<< HEAD
         self.reps: int = reps
         self.sets: int = sets
     
@@ -10,6 +9,3 @@ class Workout:
         return f"{self.name}, {self.weight}, {self.reps}, {self.sets}"
     
     def 
-=======
-        self.reps: int = reps
->>>>>>> 70708e886894c66385d0e1c943ca9041bd4a3d48

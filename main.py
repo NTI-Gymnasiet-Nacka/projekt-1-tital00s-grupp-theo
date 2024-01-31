abf7c692c42285ee64a89ce012a7682ff4d0942c
Theo LR <theo.larssonrehnstrom@elev.ga.ntig.se>
2024-01-31 10:27:03 +0100

    snabb_början

---
+++ main.py
@@ -0,0 +1,22 @@
+import csv, tkinter
+
+
+
+
+
+def create_window():
+    window = tkinter.Tk()
+    window.title("Fitness Tracker")
+    window.geometry("500x500")
+    window.resizable(True, True)    
+    return window
+    
+    pass
+
+def main():
+    window = create_window()
+    
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()

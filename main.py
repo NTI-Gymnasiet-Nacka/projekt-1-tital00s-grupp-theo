22adc85f90a395374e90413d633478a97ea6c415
MusseB <maximus.brandt@elev.ga.ntig.se>
2024-02-20 09:00:57 +0100

    psuh

--- main.py
+++ main.py
@@ -19,22 +19,21 @@ def enter_workout_name(window): # create a input in the window that takes in a s
     return name_entry # return the label
 
 
-def save_workout(window, name_entry): # save the workout to the workouts.csv file
-    name = name_entry.get() # get the name from the entry
-    name_entry.delete(0, END) # delete the entry
+def save_workout(window, workout_entry): # save the workout to the workouts.csv file
+    name = workout_entry.get() # get the name from the entry
+    workout_entry.delete(0, END) # delete the entry
     if name == "": # if the name is empty
         return # return
     with open("workouts.csv", "a", newline="", encoding="utf-8") as csvfile: # open the workouts.csv file
         writer = csv.writer(csvfile) # create a writer
         writer.writerow([name,0,0,0]) # write the name to the file
-    workouts_list = summon_workouts() # get the workouts from the file
-    print(workouts_list) # print the workouts
-    create_checkboxes(window, workouts_list, 200) # create the checkboxes
+    workouts_list = summon_workouts() # get the workouts from the file  
+    create_checkboxes(window, workouts_list, 200, workout_entry) # create the checkboxes
     return workouts_list # return the workouts
 
 #create a square where name is displayed
-def convert(name_entry, window): # create a button that saves the workout
-    convert_button = Button(window, text="Save", command=lambda: save_workout(window, name_entry)) # create a button
+def convert(workout_entry, window): # create a button that saves the workout
+    convert_button = Button(window, text="Save", command=lambda: save_workout(window, workout_entry)) # create a button
     convert_button.pack() # pack the button
     convert_button.place(x=300, y=300) # place the button
 
@@ -48,24 +47,37 @@ def summon_workouts(): # get the workouts from the workouts.csv file
             workouts_list.append(workout) # append the workout to the list
     return workouts_list # return the list
 
-def create_checkboxes(window, workouts_list, y_placement): # create a checkbox for each workout
+def disable_entries(workout_entry): # create a label
+    if workout_entry['state'] == "normal":
+        workout_entry.config(state= "disabled")
+    else:
+        workout_entry.config(state= "normal")
+    return workout_entry
+    
+
+def create_checkboxes(window, workouts_list, y_placement, workout_entry): # create a checkbox for each workout
+    checkbutton_list = [] # create a list
     for workout in workouts_list: # for each workout in the list
         var = IntVar() # create a variable
-        c = Checkbutton(window, text=workout.name, variable=var, onvalue=1, offvalue=0, variable=var, command=) # create a checkbox
-        c.pack() # pack the checkbox
-        c.place(x=100, y=y_placement) # place the checkbox
+        workout_check = Checkbutton(window, text=workout.name, onvalue=1, offvalue=0, command=lambda: disable_entries(workout_entry)) # create a checkbox
+        workout_check.pack() # pack the checkbox
+        workout_check.place(x=100, y=y_placement) # place the checkbox
+        checkbutton_list.append(workout_check) # append the checkbox to the list
         y_placement += 20 # increase the y placement
+        return checkbutton_list
 
 def startup(window): # create the window and the checkboxes
     workouts_list = summon_workouts() # get the workouts from the file
-    y_placement = create_checkboxes(window, workouts_list, 200) # create the checkboxes
+    workout_entry = enter_workout_name(window) # create the input
+    checkbutton_list = create_checkboxes(window, workouts_list, 200, workout_entry) # create the checkboxes
+    return workout_entry # return the input
 
 
 def main(): # create the window and the checkboxes
     window = create_window() # create the window
-    startup(window) # create the checkboxes
-    name_label = enter_workout_name(window) # create the input
-    convert(name_label, window) # create the button
+    workout_entry = startup(window) # create the checkboxes
+    workout_entry.state = "normal"
+    convert(workout_entry, window) # create the button
     window.mainloop() # run the window
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -8,14 +8,17 @@ win = Tk()
 win.geometry("700x350")
 
 def disable_entry():
-   entry.config(state= "disabled")
+    if entry['state'] == "disabled":
+        entry.config(state= "normal")
+    else:
+        entry.config(state= "disabled")
 
 # Create an entry widget
 entry=Entry(win, width= 40, font= ('Helvetica 16'))
 entry.pack(pady=20)
 
 # Create a button
-button=Button(win, text="Disable Entry", font=('Arial', 12), command=disable_entry)
+button=Checkbutton(win, text="Disable Entry", font=('Arial', 12), command=disable_entry)
 button.pack()
 
 win.mainloop()
